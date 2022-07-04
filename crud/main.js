7253e254fd508ec3e40c4aaa360b19bd89abfe74
romanlautaro <roman235713@gmail.com>
2022-07-04 15:45:50 -0300

    crud repaso

---
+++ crud/main.js
@@ -0,0 +1,20 @@
+
+function guardar(){
+
+    let nuevo_cliente = {
+        nombre:document.getElementById("inp_nombre").value,
+        apellido:document.getElementById("inp_apellido").value,
+        dni:document.getElementById("inp_dni").value
+    }
+
+    if("lista_clientes" in localStorage){
+       let grilla_clientes =  JSON.parse(localStorage.getItem("lista_clientes"))
+        grilla_clientes.push(nuevo_cliente)
+        localStorage.setItem("lista_clientes",JSON.stringify(grilla_clientes))
+    }else{
+        let grilla_clientes = []
+        grilla_clientes.push(nuevo_cliente) 
+        localStorage.setItem("lista_clientes", JSON.stringify(grilla_clientes) )
+    }   
+
+}
